eb16756afe6de7cc3489379c7b6ff0ae7ca5f077
ryan heupler <rheupler@gmail.com>
2016-08-27 10:59:19 -0700

    adds searcbar

---
+++ src/components/video_detail.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class VideoDetail extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBZAOICmYahmk5fgmWE5MLXju6jZQpYabA';
 
