8c645f615b021a4901d8d7547f3db1ac3b836541
ryan heupler <rheupler@gmail.com>
2016-09-10 11:24:06 -0700

    adds live search update

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageURL = video.snippet.thumbnails.default.url;
   return(
       <li onClick={() => onVideoSelect(video)} className="list-group-item">
